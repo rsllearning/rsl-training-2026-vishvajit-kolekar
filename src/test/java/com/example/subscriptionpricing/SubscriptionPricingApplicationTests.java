1f41504ef13adc8ccf6f4465be6dd1c60d227822
Vishvajit Kolekar <vishvajit.kolekar@rajasoftwarelabs.com>
2026-09-10 15:41:45 +0530

    INIT: Spring Boot project scaffold with Maven and JUnit 5

---
+++ src/test/java/com/example/subscriptionpricing/SubscriptionPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionpricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionPricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
